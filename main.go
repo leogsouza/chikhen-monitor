63821f74eb7b86aed0235c8b02ccc38f8c68613b
Leonardo Gonçalves <ldsz@gft.com>
2020-09-23 18:46:25 -0300

    remaining time calculation

--- main.go
+++ main.go
@@ -9,37 +9,60 @@ import (
 //	- Egg production formula: (amount * 24) - (30%)
 //		hen price / result formula # time left in hours
 
-// DAYHOURS - day hours
+// HOURSDAY - day hours
 const (
-	DAYHOURS  = 24
+	HOURSDAY  = 24
 	COMMISION = 0.3
 	HENPRICE  = 150
 )
 
-func main() {
-	var total float64
-	amount := 450
-	total = float64(amount) * DAYHOURS / 100
-	difference := total * COMMISION
-	result := total - difference
-	totalDays := HENPRICE / result
-	totalHours := totalDays * DAYHOURS
+type Hen struct {
+	name       string
+	price      int
+	eggsHour   int
+	totalBirds int
+	laidEggs   int
+	totalToBuy int
+}
 
-	fmt.Println(difference, total)
-	fmt.Println("result is :", result)
-	fmt.Println("Time to buy a new hen (days):", totalDays)
-	fmt.Println("Time to buy a new hen (hours):", totalHours)
+func main() {
+
+	hen := &Hen{"Raven Chick", 150, 10, 48, 4284, 21429}
+
+	totalHours := measureTimeProduction(hen)
+
+	fmt.Print("Production Time: ")
 	calculateDecimalToTime(totalHours)
+	fmt.Print("Remaining Time: ")
+	remainingHours := measureTimeRemaining(hen)
+	calculateDecimalToTime(remainingHours)
 }
 
 func calculateDecimalToTime(totalHours float64) {
 	var days, hours, minutes, seconds int
 	days = int(math.Floor(totalHours) / 24)
 	intpart, _ := math.Modf(totalHours)
-	hours = int(math.Floor(intpart)) % DAYHOURS
+	hours = int(math.Floor(intpart)) % HOURSDAY
 	minutes = int(math.Floor(totalHours*60)) % 60
 	seconds = int(math.RoundToEven(totalHours*3600)) % 60
 
-	fmt.Printf("%d days, %02d hours, %02d minutes e %02d seconds", days, hours, minutes, seconds)
+	fmt.Printf("%d days, %02d hours, %02d minutes e %02d seconds\n", days, hours, minutes, seconds)
 
+}
+
+func measureTimeProduction(hen *Hen) float64 {
+
+	var total float64
+	total = float64(hen.eggsHour*hen.totalBirds) * HOURSDAY / 100
+	difference := total * COMMISION
+	result := total - difference
+	totalDays := float64(hen.price) / result
+	totalHours := totalDays * HOURSDAY
+
+	return totalHours
+}
+
+func measureTimeRemaining(hen *Hen) float64 {
+
+	return (float64(hen.totalToBuy) - float64(hen.laidEggs)) / (float64(hen.totalBirds) * float64(hen.eggsHour))
 }
